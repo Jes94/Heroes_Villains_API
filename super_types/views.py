21129eab3f497e5f8fe2df85e7ffed9baf793777
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-04-18 14:05:35 -0400

    Added CRUD Functions For super_types + Registered URL With Project URLs + Added ID To The Serializer

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import SuperTypeSerializer
+from rest_framework import status
+from .models import SuperType
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def super_types_list(request):
+    if request.method == 'GET':
+        products = SuperType.objects.all()
+        serializer = SuperTypeSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_type_details(request, pk):
+    super_type = get_object_or_404(SuperType, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperTypeSerializer(super_type)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperTypeSerializer(super_type, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super_type.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
